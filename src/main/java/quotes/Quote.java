ec66012ee4cb580cb94c9d4dcc4e65994470dc0d
Roman Gebrehiwot <rttgg1@gmail.com>
2019-08-25 23:27:48 -0700

    update

--- src/main/java/quotes/Quote.java
+++ src/main/java/quotes/Quote.java
@@ -1,18 +1,61 @@
 package quotes;
 
-import java.util.ArrayList;
-
 public class Quote {
-   String[] tags;
-    String author;
-    String likes;
-    String text;
+    protected String[] tags;
+    protected String author;
+    protected String likes;
+    protected String text;
 
-    public Quote(String[] tags, String author, String likes, String text){
+    // Constructor
+    public Quote (String[] tags, String author, String likes, String text) {
         this.tags = tags;
         this.author = author;
         this.likes = likes;
         this.text = text;
+    }
+
+    public Quote() {
 
     }
+
+    public void setTags(String[] tags) {
+        this.tags = tags;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setLikes(String likes) {
+        this.likes = likes;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String[] getTags() {
+        return tags;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getLikes() {
+        return likes;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    // Returns a quote and its author
+    public String toString() {
+        String returnString = "";
+        returnString = "Author: "+ this.author + "\nQuote: "+ this.text;
+        return returnString;
+    }
+
+
 }
--- src/main/java/quotes/QuoteFromInternet.java
+++ src/main/java/quotes/QuoteFromInternet.java
@@ -1,71 +1,29 @@
 package quotes;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-
 public class QuoteFromInternet {
-    private final String text;
-    private String author;
+    private String quoteText;
+    private String quoteAuthor;
 
-    //    private static String randomQuoteFromInternet;
-    public static void main(String[] args) {
-        try {
-            //make a request to the forismatic API
-            URL url = new URL("http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            System.out.println(connection.getResponseCode());
+    public String getQuoteText() {
 
-            // saving the input as a string AND this line of code goes to the internet
-            BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            System.out.println("request came back");
-            String inputLine;
-            StringBuilder content = new StringBuilder();
-            while ((inputLine = input.readLine()) != null) {
-                content.append(inputLine);
-                System.out.println(inputLine);
-            }
-            input.close();
-
-            System.out.println(content);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println(e);
-        }
-    }
-    //quoteText":"The thoughts we choose to think are the tools we use to paint the canvas of our lives. ", "quoteAuthor":"Louise Hay ",
-    public QuoteFromInternet(String text, String author){
-        String randomQuote = getRandomQuoteFromInternet();
-        this.author = getQuoteFromInternetAuthor(randomQuote);
-        this.text = text;
-
+        return quoteText;
     }
 
-    private String getRandomQuoteFromInternet() {
-        return null;
+    public void setQuoteText(String quoteText) {
+
+        this.quoteText = quoteText;
     }
 
+    public String getQuoteAuthor() {
+        return quoteAuthor;
+    }
 
+    public void setQuoteAuthor(String quoteAuthor) {
+        this.quoteAuthor = quoteAuthor;
+    }
     public String toString(){
-        return (this.author + ", " + this.text);
-    }
-//gets the internet quote Author
-    protected static String getQuoteFromInternetAuthor(String quoteFromInternet){
-        return quoteFromInternet.split("\",")[1].split(":\"")[1];
-    }
-
- //get the internet quote text
- protected static String getQuoteFromInternetText(String quoteFromInternet){
-        return quoteFromInternet.split("\",")[1].split(":\"")[1];
- }
-
-
-    public Quote getFromInternet() {
-        return null;
+        String returnString = "";
+        returnString = "Author: " + this.quoteAuthor + "\nQuote: " + this.quoteText;
+        return returnString;
     }
 }
